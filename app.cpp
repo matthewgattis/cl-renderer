f7af5faef806a5541ee92c25b61ebf8e6bc06839
Ryan Gattis <ryangattis@outlook.com>
2021-10-21 22:57:26 -0500

    Initial commit.

---
+++ app.cpp
@@ -0,0 +1,14 @@
+#include "app.hpp"
+
+#define LOG_MODULE_NAME ("App")
+#include "log.hpp"
+
+App::App()
+{
+    LOG_INFO << "Instance created." << std::endl;
+}
+
+void App::run(const std::vector<std::string> &args)
+{
+}
+
