8ebfc6aab68ba0984730543fdae6980ebfb0ae50
Ryan Gattis <ryangattis@outlook.com>
2023-11-04 17:44:56 -0500

    Cleanup build system

--- vcpkgdependencies.cmake
+++ vcpkgdependencies.cmake
@@ -37,9 +37,52 @@ function(vcpkg_install)
 endfunction()
 
 function(vcpkg_add_dependencies)
-    message("vcpkg add dependencies ${ARGN}")
+    if(NOT DEFINED VCPKG_ROOT)
+        set(VCPKG_ROOT "${CMAKE_BINARY_DIR}/vcpkg")
+        if (DEFINED ENV{VCPKG_ROOT})
+            set(VCPKG_ROOT "$ENV{VCPKG_ROOT}")
+        endif()
+    endif()
+    message(STATUS "vcpkg_add_dependencies VCPKG_ROOT (${VCPKG_ROOT})")
+
+    if(NOT DEFINED CMAKE_TOOLCHAIN_FILE)
+        set(CMAKE_TOOLCHAIN_FILE "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+        set(CMAKE_TOOLCHAIN_FILE "${CMAKE_TOOLCHAIN_FILE}" PARENT_SCOPE)
+    endif()
+    message(STATUS "vcpkg_add_dependencies CMAKE_TOOLCHAIN_FILE (${CMAKE_TOOLCHAIN_FILE})")
+
+    if(NOT EXISTS "${VCPKG_ROOT}")
+        message("vcpkg_add_dependencies git clone to ${VCPKG_ROOT}")
+        execute_process(
+            COMMAND git clone https://github.com/microsoft/vcpkg.git ${VCPKG_ROOT})
+    endif()
+
+    if(WIN32)
+        set(vcpkg_exec "${VCPKG_ROOT}/vcpkg.exe")
+        set(vcpkg_bootstrap "${VCPKG_ROOT}/bootstrap-vcpkg.bat")
+    else()
+        set(vcpkg_exec "${VCPKG_ROOT}/vcpkg")
+        set(vcpkg_bootstrap "${VCPKG_ROOT}/bootstrap-vcpkg.sh")
+    endif()
+    message(STATUS "vcpkg_add_dependencies vcpkg_exec (${vcpkg_exec})")
+    message(STATUS "vcpkg_add_dependencies vcpkg_bootstrap (${vcpkg_bootstrap})")
+
+    if(NOT EXISTS ${vcpkg_exec})
+        message(STATUS "vcpkg_add_dependencies bootstraping")
+        if(WIN32)
+            execute_process(
+                COMMAND cmd /C ${vcpkg_bootstrap}
+                WORKING_DIRECTORY ${VCPKG_ROOT})
+        else()
+            execute_process(
+                COMMAND ${vcpkg_bootstrap}
+                WORKING_DIRECTORY ${VCPKG_ROOT})
+        endif()
+    endif()
+
+    message(STATUS "vcpkg_add_dependencies install (${ARGN})")
     execute_process(
-        COMMAND ${VCPKG_EXEC} install ${ARGN} --recurse --triplet ${VCPKG_TARGET_TRIPLET}
+        COMMAND ${vcpkg_exec} install ${ARGN} --recurse #--triplet ${VCPKG_TARGET_TRIPLET}
         WORKING_DIRECTORY ${VCPKG_ROOT}
         )
 endfunction()
